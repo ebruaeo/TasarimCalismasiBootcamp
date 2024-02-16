dcced775e19c8cfb9debe52e6e7499569c3b86fc
ebruaeo <ebruaeo@hotmail.com>
2024-02-16 15:19:29 +0300

    Pizza App Screen

---
+++ app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tasarimcalismasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Kotlin sınıfı içinde R = res klasörü
+        // XML dosyası içinde @= res klasörü
+    }
+}
